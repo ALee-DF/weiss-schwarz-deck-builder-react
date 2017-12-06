4717d955e11729d887299731d0bfeda8b3e32fad
Alexander Lee <alex8usa8@gmail.com>
2017-12-05 17:08:40 -0800

    Created header with the Weiss Schwarz Logo. #1

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+ReactDOM.render(
+  <header>
+    <a href=''><img src='weiss-schwarz-logo.png' alt='Weiss Schwarz Logo'/></a>
+  </header>,
+  document.querySelector('#root')
+)
