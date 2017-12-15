801fad4da89d7dd038391c380089a346ae03777d
Alexander Lee <alex8usa8@gmail.com>
2017-12-14 22:24:02 -0800

    Created areas for Level 0, Level 1, Level 2, Level 3, Event, and Climax Cards. #4

--- client/deck-section.js
+++ client/deck-section.js
@@ -3,6 +3,24 @@ import React from 'react'
 export default function DeckSection() {
   return (
     <section id='deck-section'>
+      <div>
+        <h1 className='card-headers'>Level 0</h1>
+      </div>
+      <div>
+        <h1 className='card-headers'>Level 1</h1>
+      </div>
+      <div>
+        <h1 className='card-headers'>Level 2</h1>
+      </div>
+      <div>
+        <h1 className='card-headers'>Level 3</h1>
+      </div>
+      <div>
+        <h1 className='card-headers'>Event</h1>
+      </div>
+      <div>
+        <h1 className='card-headers'>Climax</h1>
+      </div>
     </section>
   )
 }
